1664ccb3131192f30e1329d9407c97d498fa7b40
Pedro Henrique Ramos Martins <pedrohrmartins98@gmail.com>
2022-05-21 03:03:12 -0300

    Proof-of-Work added

--- block.go
+++ block.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"strconv"
 	"time"
-	"bytes"
-	"crypto/sha256"
 )
 
-type Block struct{
-	Timestamp int64
-	Data []byte
+// Block keeps block headers
+type Block struct {
+	Timestamp     int64
+	Data          []byte
 	PrevBlockHash []byte
-	Hash []byte
+	Hash          []byte
+	Nonce         int
 }
 
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
-}
-
-func (b *Block) SetHash()  {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
+// NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
 	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
 }
--- blockchain.go
+++ blockchain.go
@@ -1,19 +1,18 @@
 package main
 
-import (
-)
-
-type BlockChain struct{
+// Blockchain keeps a sequence of Blocks
+type Blockchain struct {
 	blocks []*Block
 }
 
-func NewBlockchain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesisBlock()}}
+// AddBlock saves provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
 }
 
-func (bc *BlockChain) AddBlock(data string)  {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	NewBlock := NewBlock(data, prevBlock.Hash)
-
-	bc.blocks = append(bc.blocks, NewBlock)
+// NewBlockchain creates a new Blockchain with genesis Block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
